0af03d82ec6379de86ee26db258bbe1aed9faa01
Anthony <orphynou@icloud.com>
2024-03-14 15:55:36 +0100

    Mise a jour package.json, index.html et yarn.lock, suppression des fichiers inutiles

--- src/pages/Apropos/index.jsx
+++
@@ -1,9 +0,0 @@
-function Apropos() {
-  return (
-    <div>
-      <h1>A propos</h1>
-    </div>
-  )
-}
-
-export default Apropos
--- src/pages/Fiches/index.jsx
+++
@@ -1,9 +0,0 @@
-function Fiches() {
-  return (
-    <div>
-      <h1>Fiches</h1>
-    </div>
-  )
-}
-
-export default Fiches

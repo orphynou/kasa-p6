2f3c7f98479ff4c0b98cd67a7528ec68ab85ae62
Anthony <orphynou@icloud.com>
2024-03-14 15:41:22 +0100

    Ajout de la page d'erreur

---
+++ src/pages/Error/error.jsx
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom'
+import './error.scss'
+
+function Error() {
+  return (
+    <main className="main__error">
+      <p className="error__404">404</p>
+      <p className="error__txt">
+        Oups! La page que vous demandez n'existe pas.
+      </p>
+      <Link to="/" className="error___link">
+        Retourner sur la page d’accueil
+      </Link>
+    </main>
+  )
+}
+
+export default Error
